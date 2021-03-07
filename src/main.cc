006fdaa8ebf8b854b56cffea0e7bd75123132d77
tiger <tiger@xiedeacc.com>
2021-03-07 15:24:32 -0800

    update

---
+++ src/main.cc
@@ -0,0 +1,16 @@
+#include "src/void_data_maker.h"
+#include <vector>
+using std::vector;
+using protobuf_plugin_demo::VoidDataMaker;
+using foo:Foo;
+using bar::Bar;
+using protobuf_plugin_demo::BaseTypeMemory;
+
+int main(const int argc, const char** argv) {
+  vector<int> field_ids;
+  Foo foo_fields;
+  vector<const void *> void_data;
+  BaseTypeMemory memory_holder;
+  VoidDataMaker.Build(field_ids, foo_fields, &void_data, & memory_holder);
+  return 0;
+}
